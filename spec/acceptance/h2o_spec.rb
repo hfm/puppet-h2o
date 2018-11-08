5fb4f29e0757e56e14eb11c23b62ed4f7ac79efa
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-11-09 01:03:51 +0900

    Run `rake rubocop:auto_correct`

--- spec/acceptance/h2o_spec.rb
+++ spec/acceptance/h2o_spec.rb
@@ -8,34 +8,34 @@ describe 'h2o class' do
     EOS
   end
 
-  it 'should work without errors' do
+  it 'works without errors' do
     expect(apply_manifest(manifest, catch_failures: true).exit_code).to eq 2
   end
 
-  it 'should run a second time without changes' do
+  it 'runs a second time without changes' do
     expect(apply_manifest(manifest).exit_code).to be_zero
   end
 
   describe yumrepo('h2o'), if: os[:family] == 'redhat' do
-    it { should be_enabled }
+    it { is_expected.to be_enabled }
   end
 
   describe package('h2o') do
-    it { should be_installed }
+    it { is_expected.to be_installed }
   end
 
   describe file('/etc/h2o/h2o.conf') do
-    it { should be_file }
+    it { is_expected.to be_file }
   end
 
   describe service('h2o') do
-    it { should be_enabled }
-    it { should be_running }
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_running }
   end
 
   [80, 443].each do |num|
     describe port(num) do
-      it { should be_listening.on('0.0.0.0') }
+      it { is_expected.to be_listening.on('0.0.0.0') }
     end
   end
 end
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 describe 'h2o' do
   context 'with default values for all parameters (redhat)' do
-    it { should compile }
-    it { should contain_class('h2o') }
+    it { is_expected.to compile }
+    it { is_expected.to contain_class('h2o') }
   end
 
   context 'with default values for all parameters (debian)' do
@@ -12,17 +12,17 @@ describe 'h2o' do
           family: 'Debian',
           name: 'Ubuntu',
           distro: {
-            codename: 'xenial'
+            codename: 'xenial',
           },
           release: {
-            full: '18.04'
-          }
+            full: '18.04',
+          },
         },
-        osfamily: 'Debian'
+        osfamily: 'Debian',
       }
     end
 
-    it { should compile }
-    it { should contain_class('h2o') }
+    it { is_expected.to compile }
+    it { is_expected.to contain_class('h2o') }
   end
 end
